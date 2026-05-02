254852f619502d7833435d1ab6bba07a4a69ac75
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2026-05-02 10:22:39 +0200

    implem header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import { getActivity, getUser } from "../mockedApi";
+import logoSportSee from "../assets/logoSportSee.svg";
+
+const links = ["Acceuil", "Profil", "Réglage", "Communauté"];
 
 const dataUser = await getUser(12);
 console.log("coucou", dataUser.userInfos.firstName);
@@ -7,7 +10,20 @@ const dataActivity = await getActivity(12);
 console.log("activity", dataActivity.sessions[0].day);
 
 const Header = () => {
-  return <div className="container-header">header</div>;
+  return (
+    <div className="header">
+      <div className="header__logo-wrapper">
+        <img className="header__logo" src={logoSportSee} alt="logo site" />y
+      </div>
+      <nav className="header__nav">
+        {links.map((link, index) => (
+          <a key={`${index}-${link}`} className="header__navLink" href="">
+            {link}
+          </a>
+        ))}
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
