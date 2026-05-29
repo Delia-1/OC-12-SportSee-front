b4db645e69c24a894d73b6202b6826315ce3ffca
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2026-05-29 15:30:50 +0200

    implem daily activity diagram

--- src/components/graphs/DailyScore.jsx
+++ src/components/graphs/DailyScore.jsx
@@ -1,8 +1,105 @@
+import { Pie, PieChart, Cell, ResponsiveContainer } from "recharts";
+
 const DailyScore = ({ todayScore }) => {
+  todayScore = 1;
+
+  const data = [
+    { name: "score", value: todayScore },
+    { name: "rest", value: 1 - todayScore },
+  ];
+
   return (
-    <div>
-      <h3>Daily score:</h3>
-      <p>{`score.: ${todayScore}`}</p>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        aspectRatio: 1,
+        backgroundColor: "#FBFBFB",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          inset: "5px",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "8px",
+            left: "24px",
+            fontFamily: "roboto",
+            fontSize: "26px",
+          }}
+        >
+          <span
+            style={{
+              fontFamily: "roboto",
+              fontSize: "15px",
+              fontWeight: "500",
+              color: "#20253A",
+            }}
+          >
+            Score
+          </span>
+        </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              innerRadius="60%"
+              outerRadius="70%"
+              cornerRadius="50%"
+              paddingAngle={5}
+              startAngle={90}
+              endAngle={450}
+            >
+              <Cell fill="#FF0000" stroke="none" />
+              <Cell fill="transparent" stroke="none" />
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "59%",
+            height: "59%",
+            borderRadius: "50%",
+            backgroundColor: "#FFFFFF",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            pointerEvents: "none",
+          }}
+        >
+          <span
+            style={{
+              fontFamily: "roboto",
+              fontSize: "26px",
+              fontWeight: "700",
+            }}
+          >
+            {todayScore * 100}%
+          </span>
+
+          <span
+            style={{
+              color: "#74798C",
+              fontFamily: "roboto",
+              fontSize: "16px",
+              fontWeight: "500",
+            }}
+          >
+            de votre
+            <br /> objectif
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
