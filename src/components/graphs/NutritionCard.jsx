9a6ef4e2cafe52ad92e97e0a6c22ef7ad1be2120
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2026-06-05 07:46:47 +0200

    implem nutrition cards

--- src/components/graphs/NutritionCard.jsx
+++ src/components/graphs/NutritionCard.jsx
@@ -6,23 +6,29 @@ import chicken from "../../assets/chicken.svg";
 const NutritionCard = ({ amount, type }) => {
   let name = "";
   let icon = "";
+  let bgColor = "";
 
   switch (type) {
     case "calorieCount":
       name = "Calories";
       icon = energy;
+      bgColor = "#FF00001A";
       break;
     case "proteinCount":
       name = "Proteines";
       icon = chicken;
+      bgColor = "#4AB8FF1A";
       break;
     case "carbohydrateCount":
       name = "Glucides";
       icon = apple;
+      bgColor = "#F9CE231A";
       break;
     case "lipidCount":
       name = "Lipides";
       icon = cheeseburger;
+      bgColor = "#FD51811A";
+
       break;
     default:
       console.log(`Donnée nutritionnelle inconnue: ${type}`);
@@ -30,7 +36,12 @@ const NutritionCard = ({ amount, type }) => {
 
   return (
     <div className="nutrition-card">
-      <img src={icon} alt={name} />
+      <div
+        className="nutrition-card__icon-container"
+        style={{ backgroundColor: bgColor }}
+      >
+        <img src={icon} alt={name} />
+      </div>
       <div className="nutrition-card__infos">
         <p className="amount">{amount}</p>
         <p className="type">{name}</p>
